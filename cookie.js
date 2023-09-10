c229c3f40f44737a166ad1c96d1d63f5bf0c7442
Altheo <altheo.pacheco@irc.pshs.edu.ph>
2023-09-10 18:57:28 +0800

    added cookie form

--- cookie.js
+++ cookie.js
@@ -1,5 +1,13 @@
-console.log(navigator.cookieEnabled);
-
 function getInfo() {
-    console.log("info");
+    let name = document.querySelector("#name").value;
+    let contactNumber = document.querySelector("#number").value;
+    console.log(name + contactNumber);
+    setCookie("user", name, 1);
+}
+
+function setCookie(name, value, exp) {
+    let date = new Date();
+    date.setTime(date.getTime + (exp * 24 * 60 * 60 * 1000));
+    let expires_UTC = "expires=" + date.toUTCString();
+    document.cookie = name + "=" + value + ";" + expires_UTC + ";";
 }
