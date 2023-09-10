098415ee64732f4d19cae79f3a4f5f351a1d7ff5
Altheo <altheo.pacheco@irc.pshs.edu.ph>
2023-09-10 18:45:56 +0800

    cookie2

--- cookie.js
+++ cookie.js
@@ -1,7 +1,5 @@
 console.log(navigator.cookieEnabled);
-document.cookie = "username=John Smith; expires=Thu, 18 Dec 2013 12:00:00 UTC; path=/";
-let cook = document.cookie
-console.log(cook);
 
 function getInfo() {
+    console.log("info");
 }
